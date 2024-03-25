3f4bdb16761cdd67a40ea3e063eb8bcc726282d0
vjeromel276 <49462377+vjeromel276@users.noreply.github.com>
2024-03-25 12:01:37 -0400

    init push

--- chapter2/web/web-example/src/index.js
+++ chapter2/web/web-example/src/index.js
@@ -1,3 +1,6 @@
 import * as tf from "@tensorflow/tfjs";
 
 console.log(tf.version.tfjs);
+
+const app = document.getElementById("app");
+app.innerHTML = `<h1>TensorFlow.js version: ${tf.version.tfjs}</h1>`;
